287330572ab8cdbc71c659425d6d36332c422ef0
krote <krote0924@gmail.com>
2022-05-24 23:52:42 +0900

    addd liff app

--- index.js
+++ index.js
@@ -4,72 +4,74 @@ const line = require('@line/bot-sdk');
 
 //Postgresへの接続
 const connection = new Client({
- connectionString: process.env.DATABASE_URL,
- ssl: {
-   rejectUnauthorized: false
- }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 connection.connect();
 
 //usersテーブル作成クエリ
 const create_query = {
- text: 'CREATE TABLE IF NOT EXISTS users (id SERIAL NOT NULL, line_uid VARCHAR(50), name VARCHAR(20), age SMALLINT);'
+    text: 'CREATE TABLE IF NOT EXISTS users (id SERIAL NOT NULL, line_uid VARCHAR(50), name VARCHAR(20), age SMALLINT);'
 };
 
 //CREATEクエリ実行
 connection.query(create_query)
- .then(()=>console.log('usersテーブル作成成功！！'))
- .catch(e=>console.log(e));
- 
+    .then(() => console.log('usersテーブル作成成功！！'))
+    .catch(e => console.log(e));
+
 const PORT = process.env.PORT || 5000;
 
 const config = {
- channelAccessToken:process.env.ACCESS_TOKEN,
- channelSecret:process.env.CHANNEL_SECRET
+    channelAccessToken: process.env.ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
 };
 const client = new line.Client(config);
 
 //express
 express()
- .post('/hook',line.middleware(config),(req,res)=> lineBot(req,res))
- .listen(PORT,()=>console.log(`Listening on ${PORT}`));
+    .use(express.static('public'))
+    .post('/hook', line.middleware(config), (req, res) => lineBot(req, res))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const lineBot = (req,res) => {
- res.status(200).end();
- const events = req.body.events;
- const promises = [];
- for(let i=0;i<events.length;i++){
-     const ev = events[i];
-     switch(ev.type){
-       case 'follow':
-         promises.push(greeting_follow(ev));
-         break;
-       case 'unfollow':
-         console.log('unfollow');
-       case 'message':
-         console.log('message');
-         break;
-     }
- }
- Promise
-   .all(promises)
-   .then(console.log('all promises passed'))
-   .catch(e=>console.error(e.stack));
+const lineBot = (req, res) => {
+    res.status(200).end();
+    const events = req.body.events;
+    const promises = [];
+    for (let i = 0; i < events.length; i++) {
+        const ev = events[i];
+        switch (ev.type) {
+            case 'follow':
+                promises.push(greeting_follow(ev));
+                break;
+            case 'unfollow':
+                console.log('unfollow');
+                break;
+            case 'message':
+                console.log('message');
+                break;
+        }
+    }
+    Promise
+        .all(promises)
+        .then(console.log('all promises passed'))
+        .catch(e => console.error(e.stack));
 }
 
 //フォローしたら挨拶を返し、更にテーブルへユーザー情報を格納する
 const greeting_follow = async (ev) => {
     const profile = await client.getProfile(ev.source.userId);
     const insert_query = {
-      text: `INSERT INTO users (line_uid,name,age) VALUES($1,$2,$3);`,
-      values: [ev.source.userId,profile.displayName,33]
+        text: `INSERT INTO users (line_uid,name,age) VALUES($1,$2,$3);`,
+        values: [ev.source.userId, profile.displayName, 33]
     };
     connection.query(insert_query)
-      .then(()=>{
-        return client.replyMessage(ev.replyToken,{
-          "type":"text",
-          "text":`${profile.displayName}さん、フォローありがとうございます\uDBC0\uDC04`
-        });
-      })
-      .catch(e=>console.log(e));
-   }
+        .then(() => {
+            return client.replyMessage(ev.replyToken, {
+                "type": "text",
+                "text": `${profile.displayName}さん、フォローありがとうございます\uDBC0\uDC04`
+            });
+        })
+        .catch(e => console.log(e));
+}
