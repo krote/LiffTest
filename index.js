1282c8abbe5538e87a783e5308da720e279cd2ea
krote <krote0924@gmail.com>
2022-05-24 17:01:54 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { Client } = require('pg');
+
+// Postgresへの接続
+const connection = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+connection.connect();
+
+// usersテーブル作成クエリ
+const create_query = {
+    text: 'CREATE TABLE IF NOT EXISTS users(id SERIAL NOT NULL, line_uid VARCHAR(50), name'
+};
+
+// CREATE 実行
+connection.query(create_query)
+    .then(()=>console.log('usersテーブル作成成功'))
+    .catch(e=>console.log(e));
+
+const PORT = rocess.env.PORT || 5000;
+
+express()
+    .listen(PORT, ()=>console.log('Listening on ${PORT}'));
+
